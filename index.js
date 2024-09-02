97b8da2b2f0296f60e03d0bf460a1c5c2da71108
Ahmad Zaki <166486850+ahmadzakiyox@users.noreply.github.com>
2024-09-02 18:33:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ const productSchema = new mongoose.Schema({
 const userSchema = new mongoose.Schema({
   userId: { type: Number, required: true, unique: true },
   saldo: { type: Number, default: 0 },
+  email: { type: String, unique: true, sparse: true }, // email bisa null dan unik
 });
 
 // Schema Stock, sesuaikan dengan model Anda
